adaf937944ebcb6f8e39c46431e65d8c3843a75c
Tom Dawes <tomdawes91@gmail.com>
2020-06-03 14:23:28 +0100

    Git lola

--- git.sh
+++ git.sh
@@ -6,3 +6,4 @@ git config --global user.email "tomdawes91@gmail.com"
 git config --global alias.pull-request "! ${${(%):-%x}:a:h}/git/create-pull-request"
 git config --global alias.pr pull-request
 git config --global alias.uplink "! ${${(%):-%x}:a:h}/git/uplink"
+git config --global alias.lola "log --oneline --all --decorate --graph"
